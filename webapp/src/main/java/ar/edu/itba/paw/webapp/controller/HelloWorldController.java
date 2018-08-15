8a8b843d246e53be269a3d8a03ce2a3e0fa178d1
Francisco Delgado <fdelgado@itba.edu.ar>
2018-08-14 22:46:53 -0300

    Add first controller

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package ar.edu.itba.paw.webapp.controller;
+
+//TODO: Creo que este UserService deberia estar en un paquete que se llama interface.Userservice
+import ar.edu.itba.paw.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @Autowired
+    private UserService us;
+
+    @RequestMapping("/")
+    public ModelAndView helloWorld(){
+        final ModelAndView mav = new ModelAndView("index");
+        mav.addObject("user", us.findUserById(0));
+        return mav;
+    }
+
+}
