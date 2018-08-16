d7c312d4cd8b74b9614d5a480adefc6fc53ed85b
Francisco Delgado <fdelgado@itba.edu.ar>
2018-08-16 01:03:52 -0300

    Moved UserDao to interfaces

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/UserDao.java
@@ -1,5 +1,5 @@
 package ar.edu.itba.paw.persistance;
-
+//TODO: estos paquetes deberian ser de  interfaces.persistance y no solamente persistance no?
 import ar.edu.itba.paw.model.User;
 
 public interface UserDao {
