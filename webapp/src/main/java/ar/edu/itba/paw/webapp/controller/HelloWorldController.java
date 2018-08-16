a47eb28a57e3fcfce540899948aba0bcb6d28979
Francisco Delgado <fdelgado@itba.edu.ar>
2018-08-16 01:13:18 -0300

    Add qualifier for HelloWorld Controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package ar.edu.itba.paw.webapp.controller;
 //TODO: Creo que este UserService deberia estar en un paquete que se llama interface.Userservice
 import ar.edu.itba.paw.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +15,7 @@ public class HelloWorldController {
     private UserService us;
 
     @RequestMapping("/")
+    @Qualifier("userServiceImpl")
     public ModelAndView helloWorld(){
         final ModelAndView mav = new ModelAndView("index");
         mav.addObject("user", us.findUserById(0));
