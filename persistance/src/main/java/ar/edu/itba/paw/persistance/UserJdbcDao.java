90dfe65ebffe04965b4363e6fffe06b2cf4a7382
Francisco Delgado <fdelgado@itba.edu.ar>
2018-08-16 01:07:23 -0300

    Update dependancy packages on the UserJdbcDao

--- persistance/src/main/java/ar/edu/itba/paw/persistance/UserJdbcDao.java
+++ persistance/src/main/java/ar/edu/itba/paw/persistance/UserJdbcDao.java
@@ -1,10 +1,11 @@
 package ar.edu.itba.paw.persistance;
 
-import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.interfaces.User;
+import ar.edu.itba.paw.interfaces.UserDao;
 
 
 //TODO: Falta la dependencia de spring
-@Repository
+//@Repository
 public class UserJdbcDao implements UserDao {
     @Override
     public User findById(long id) {
