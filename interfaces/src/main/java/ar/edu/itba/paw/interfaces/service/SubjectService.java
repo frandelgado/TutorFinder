77c8e86f667e99423a760e98fa368e56a1f7158f
Francisco Delgado <fdelgado@itba.edu.ar>
2018-08-29 14:17:04 -0300

    Add create method to SubjectService interface

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/SubjectService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/SubjectService.java
@@ -1,4 +1,10 @@
 package ar.edu.itba.paw.interfaces.service;
 
+import ar.edu.itba.paw.models.Subject;
+
 public interface SubjectService {
+    Subject findSubjectById(final long id);
+
+    Subject create(final String name, final String description);
+
 }
