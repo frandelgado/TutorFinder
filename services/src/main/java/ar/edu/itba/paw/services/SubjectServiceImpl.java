6d951301f25e68f207fc08d03cff08ac083d7c35
Francisco Delgado <fdelgado@itba.edu.ar>
2018-08-29 14:19:46 -0300

    Add create method implementation to SubjectServiceImpl

--- services/src/main/java/ar/edu/itba/paw/services/SubjectServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/SubjectServiceImpl.java
@@ -1,4 +1,27 @@
 package ar.edu.itba.paw.services;
 
-public class SubjectServiceImpl {
+import ar.edu.itba.paw.interfaces.persistence.SubjectDao;
+import ar.edu.itba.paw.interfaces.service.SubjectService;
+import ar.edu.itba.paw.models.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubjectServiceImpl implements SubjectService {
+
+
+    @Autowired
+    private SubjectDao subjectDao;
+
+    @Override
+    public Subject findSubjectById(long id) {
+        return null;
+    }
+
+    @Override
+    public Subject create(String name, String description) {
+        return subjectDao.create(name,description);
+    }
+
+
 }
