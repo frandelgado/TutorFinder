5755b7e097ee4d88330b9784db4dc3fc49bb17b4
Francisco Delgado <fdelgado@itba.edu.ar>
2018-08-29 14:21:36 -0300

    Add createSubject mapping to controller

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -3,10 +3,13 @@ package ar.edu.itba.paw.webapp.controller;
 //TODO: Creo que este UserService deberia estar en un paquete que se llama interface.Userservice
 
 import ar.edu.itba.paw.interfaces.service.UserService;
+import ar.edu.itba.paw.interfaces.service.SubjectService;
+import ar.edu.itba.paw.models.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,12 +19,27 @@ public class HelloWorldController {
     @Qualifier("userServiceImpl")
     private UserService us;
 
+    @Autowired
+    @Qualifier("subjectServiceImpl")
+    private SubjectService ss;
+
     @RequestMapping("/")
     @Qualifier("userServiceImpl")
     public ModelAndView helloWorld(){
         final ModelAndView mav = new ModelAndView("index");
         mav.addObject("user", us.findUserById(0));
         return mav;
+    }
+
+    @RequestMapping("/createSubject")
+    @Qualifier("subjectServiceImpl")
+    public ModelAndView create(
+            @RequestParam(value="name", required=true) final String name,
+            @RequestParam(value="description", required=true) final String description
+        )
+    {
+        final Subject s = ss.create(name, description);
+        return new ModelAndView("redirect:/Subject/?subjectId=" + s.getId());
     }
 
 }
