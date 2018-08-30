3679a3856ad6aa08d190d2445e9e2df8cc0994ca
Francisco Delgado <fdelgado@itba.edu.ar>
2018-08-29 21:20:29 -0300

    Fix subjectService findById

--- services/src/main/java/ar/edu/itba/paw/services/SubjectServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/SubjectServiceImpl.java
@@ -13,9 +13,10 @@ public class SubjectServiceImpl implements SubjectService {
     @Autowired
     private SubjectDao subjectDao;
 
+    //TODO: I dont think it is a good idea to return null if there is no user, maybe an exception?
     @Override
     public Subject findSubjectById(long id) {
-        return null;
+        return subjectDao.findById(id).orElse(null);
     }
 
     @Override
