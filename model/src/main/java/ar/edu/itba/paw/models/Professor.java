1d1c016a5d8e7044f034261b5ae8c9fad726eed7
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-08 05:04:55 -0300

    Add Professor fields

--- model/src/main/java/ar/edu/itba/paw/models/Professor.java
+++ model/src/main/java/ar/edu/itba/paw/models/Professor.java
@@ -2,4 +2,19 @@ package ar.edu.itba.paw.models;
 
 public class Professor {
 
+    private final User user;
+    private final String description;
+
+    public Professor(User user, String description) {
+        this.user = user;
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
