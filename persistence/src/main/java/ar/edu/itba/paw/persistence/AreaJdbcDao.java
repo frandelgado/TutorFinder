aca65a90602262a5a0f784f4b39fe3eed1644b75
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-08 04:59:03 -0300

    Add AreaJdbcDao

---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/AreaJdbcDao.java
@@ -0,0 +1,61 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.interfaces.persistence.AreaDao;
+import ar.edu.itba.paw.models.Area;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class AreaJdbcDao implements AreaDao {
+
+    private JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
+
+    private final static RowMapper<Area> ROW_MAPPER = (rs, rowNum) -> new Area(
+            rs.getLong("area_id"),
+            rs.getString("description"),
+            rs.getString("name")
+    );
+
+    @Autowired
+    public AreaJdbcDao(final DataSource ds) {
+        jdbcTemplate = new JdbcTemplate(ds);
+        jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("areas")
+                .usingGeneratedKeyColumns("area_id");
+    }
+    @Override
+    public Optional<Area> findById(final long id) {
+        final List<Area> list = jdbcTemplate.query(
+                "SELECT * FROM areas WHERE area_id = ?", ROW_MAPPER, id
+        );
+        return list.stream().findFirst();
+    }
+
+    @Override
+    public Area create(final String name, final String description) {
+        final Map<String, Object> args = new HashMap<>();
+        args.put("name", name);
+        args.put("description", description);
+        final Number areaId = jdbcInsert.executeAndReturnKey(args);
+        return new Area(areaId.longValue(), description, name);
+    }
+
+    @Override
+    public List<Area> filterAreasByName(String name) {
+        final String search = "%" + name + "%";
+        final List<Area> list = jdbcTemplate.query(
+                "SELECT * FROM areas WHERE UPPER(name) LIKE UPPER(?)", ROW_MAPPER, search
+        );
+        return list;
+    }
+}
