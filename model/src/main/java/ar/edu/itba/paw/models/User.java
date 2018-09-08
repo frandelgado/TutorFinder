04e741256e21135612eb9755fd8ece5ad1195f1a
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-08 05:04:17 -0300

    Add User fields

--- model/src/main/java/ar/edu/itba/paw/models/User.java
+++ model/src/main/java/ar/edu/itba/paw/models/User.java
@@ -2,14 +2,44 @@ package ar.edu.itba.paw.models;
 
 public class User {
     private final Long id;
+    private final String username;
     private final String name;
+    private final String lastname;
     //TODO: la pass deberia estar hasheada no en plaintext
     private final String password;
+    private final String email;
 
 
-    public User(Long id, String name, String password) {
+    public User(Long id, String username, String name, String lastname, String password, String email) {
         this.id = id;
+        this.username = username;
         this.name = name;
+        this.lastname = lastname;
         this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
