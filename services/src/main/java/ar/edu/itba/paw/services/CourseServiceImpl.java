a0b01174cc4e9a8f937e6eef9fcfee86e8352365
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-10 11:36:05 -0300

    Add Course service implementation

---
+++ services/src/main/java/ar/edu/itba/paw/services/CourseServiceImpl.java
@@ -0,0 +1,29 @@
+package ar.edu.itba.paw.services;
+
+import ar.edu.itba.paw.interfaces.persistence.CourseDao;
+import ar.edu.itba.paw.interfaces.service.CourseService;
+import ar.edu.itba.paw.models.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+
+    @Autowired
+    private CourseDao courseDao;
+
+    //TODO: I dont think it is a good idea to return null if there is no user, maybe an exception?
+    @Override
+    public Course findCourseByIds(long professor_id, long subject_id) {
+        return courseDao.findByIds(professor_id, subject_id).orElse(null);
+    }
+
+    @Override
+    public List<Course> filterCoursesByName(final String name){
+        return courseDao.filterCoursesByName(name);
+    }
+
+}
