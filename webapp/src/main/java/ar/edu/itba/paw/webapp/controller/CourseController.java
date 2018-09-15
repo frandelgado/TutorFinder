8b0f59fc7640afec804e422c61857505155cc0b0
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-15 08:55:25 -0300

    Add CreateCourse post and get
    Todo: replace id professor with logged user id

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/CourseController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/CourseController.java
@@ -1,12 +1,23 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.interfaces.service.CourseService;
+import ar.edu.itba.paw.interfaces.service.ProfessorService;
+import ar.edu.itba.paw.interfaces.service.SubjectService;
+import ar.edu.itba.paw.models.Course;
+import ar.edu.itba.paw.models.Professor;
+import ar.edu.itba.paw.models.Subject;
+import ar.edu.itba.paw.webapp.form.CourseForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -14,8 +25,15 @@ public class CourseController {
 
     @Autowired
     @Qualifier("courseServiceImpl")
-    private CourseService cs;
+    private CourseService courseService;
 
+    @Autowired
+    @Qualifier("subjectServiceImpl")
+    private SubjectService subjectService;
+
+    @Autowired
+    @Qualifier("professorServiceImpl")
+    private ProfessorService professorService;
 
     @RequestMapping("/Course")
     public ModelAndView course(
@@ -23,7 +41,32 @@ public class CourseController {
             @RequestParam(value="subject", required=true) final Long subject_id
     ){
         final ModelAndView mav = new ModelAndView("course");
-        mav.addObject("course", cs.findCourseByIds(professor_id, subject_id));
+        mav.addObject("course", courseService.findCourseByIds(professor_id, subject_id));
         return mav;
     }
+
+    @RequestMapping("/createCourse")
+    public ModelAndView createCourse(@ModelAttribute("CourseForm") final CourseForm form) {
+        final ModelAndView mav = new ModelAndView("createCourse");
+        mav.addObject("subjects", subjectService.filterSubjectsByName(""));
+        return mav;
+    }
+
+    @RequestMapping(value = "/createCourse", method = RequestMethod.POST)
+    public ModelAndView create(@Valid @ModelAttribute("CourseForm") final CourseForm form,
+                               final BindingResult errors) {
+        if(errors.hasErrors()) {
+            return createCourse(form);
+        }
+
+        final Subject subject = subjectService.findSubjectById(form.getSubjectId());
+
+        //TODO: Catch null && replace professor null
+
+        final Course course = courseService.create(null, subject, form.getDescription(), form.getPrice());
+
+        return new ModelAndView("redirect:/Course/?professor=" + course.getProfessor().getId() +
+                "&subject=" + course.getSubject().getId());
+    }
+
 }
--- webapp/src/main/webapp/WEB-INF/jsp/course.jsp
+++ webapp/src/main/webapp/WEB-INF/jsp/course.jsp
@@ -11,17 +11,7 @@
 
 <body class="staticClass">
 
-<div class="navbar">
-    <a href="<c:url value="/" />" class="logo-box">
-        <img alt="Tu Teoria" class="logo" src="<c:url value="/resources/images/logo_invert.jpg" />" />
-    </a>
-    <!-- 
-    <div class="navbar-buttons">
-        <button class="navbar-button"><spring:message code="register"/></button>
-        <button class="navbar-button"><spring:message code="login"/></button>
-    </div>
-    -->
-</div>
+<%@ include file="navbar.jsp" %>
 
 <div class="content">
     <h1 class="title"><c:out value="${course.subject.name} - ${course.subject.name}" escapeXml="true"/></h1>
