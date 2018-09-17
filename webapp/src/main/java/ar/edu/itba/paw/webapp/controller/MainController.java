bc00788278965ce72bb737f899d8cf070d0d4fa6
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-16 23:00:52 -0300

    Add initial version of authentication

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MainController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MainController.java
@@ -4,8 +4,11 @@ import ar.edu.itba.paw.interfaces.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -20,6 +23,5 @@ public class MainController {
         mav.addObject("user", us.findUserById(0));
         return mav;
     }
-
 
 }
