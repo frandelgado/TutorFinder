be936a07ac53a46eb95264cef5f9fee0b0095756
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-16 22:54:58 -0300

    Add initial version of course creation form

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/CourseForm.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.paw.webapp.form;
+
+import javax.validation.constraints.NotNull;
+
+public class CourseForm {
+
+    private String description;
+    private Integer price;
+
+}
