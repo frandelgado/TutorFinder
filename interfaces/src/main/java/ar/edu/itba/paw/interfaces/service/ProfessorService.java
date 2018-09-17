7a62b11cc89cc8282bb28450e55c40b9b6239049
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-16 22:56:31 -0300

    Add new methods to services interfaces

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ProfessorService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ProfessorService.java
@@ -4,5 +4,12 @@ import ar.edu.itba.paw.models.Professor;
 
 public interface ProfessorService {
 
+    Professor findById(final Long id);
+
+    Professor findByUsername(final String username);
+
     Professor create(final Long userId, final String description);
+
+    Professor createWithUser(final Long id, final String username, final String name, final String lastname,
+                             final String password, final String email, final String description);
 }
--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/UserService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/UserService.java
@@ -3,7 +3,9 @@ import ar.edu.itba.paw.models.User;
 
 public interface UserService {
 
-    User findUserById(long id);
+    User findUserById(final long id);
+
+    User findByUsername(final String username);
 
     User create(final String username, final String password,
                 final String email, final String name, final String lastName);
