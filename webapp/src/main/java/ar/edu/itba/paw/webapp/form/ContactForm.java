d3faa49a378bab3bf013cab635a0296bc11a0b8c
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-19 15:46:31 -0300

    Add contactProfessor form

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/ContactForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/ContactForm.java
@@ -12,4 +12,20 @@ public class ContactForm {
     @NotNull
     @Size(min = 1, max = 512)
     private String body;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
