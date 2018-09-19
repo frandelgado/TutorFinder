d52ead025e257d541d5f8983c8c6db00a5ab1b2c
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-19 14:19:42 -0300

    Add send mail to proferssor interface

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/EmailService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/EmailService.java
@@ -1,6 +1,9 @@
 package ar.edu.itba.paw.interfaces.service;
 
+import ar.edu.itba.paw.models.Professor;
+
 public interface EmailService {
 
     public void sendMessage(String to, String subject, String text);
+    public void sendMailToProfessor(Professor professor, String subject, String text);
 }
