cd561bcfd3b885d7645eaead6ef4d425c5c4f9f2
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-19 03:59:08 -0300

    Add Profiles

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
@@ -1,5 +1,6 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import ar.edu.itba.paw.interfaces.service.CourseService;
 import ar.edu.itba.paw.interfaces.service.ProfessorService;
 import ar.edu.itba.paw.interfaces.service.UserService;
 import ar.edu.itba.paw.models.Professor;
@@ -7,9 +8,12 @@ import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.webapp.form.RegisterForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,6 +29,10 @@ public class UserController {
     @Autowired
     @Qualifier("professorServiceImpl")
     private ProfessorService ps;
+
+    @Autowired
+    @Qualifier("courseServiceImpl")
+    private CourseService cs;
 
     @RequestMapping("/register")
     public ModelAndView register(@ModelAttribute("registerForm") final RegisterForm form) {
@@ -47,4 +55,25 @@ public class UserController {
         return new ModelAndView("login");
     }
 
+    @RequestMapping("/Professor/{id}")
+    public ModelAndView professorProfile(@PathVariable(value = "id") long id) {
+        final ModelAndView mav = new ModelAndView("profile");
+        mav.addObject("courses", cs.findCourseByProfessorId(id));
+        mav.addObject("professor", ps.findById(id));
+        return mav;
+    }
+
+    @RequestMapping("/Profile")
+    public ModelAndView profile() {
+        final ModelAndView mav = new ModelAndView("profileForProfessor");
+
+        final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        final String username = auth.getName();
+
+        final Professor professor = ps.findByUsername(username);
+
+        mav.addObject("courses", cs.findCourseByProfessorId(professor.getId()));
+        mav.addObject("professor", professor);
+        return mav;
+    }
 }
