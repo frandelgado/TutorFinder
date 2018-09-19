91b6993e23ee18bba5947bf17a7d7f7516820c1b
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-19 14:20:05 -0300

    Add implementation of send email to professor service

--- services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.service.EmailService;
+import ar.edu.itba.paw.models.Professor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -17,6 +18,11 @@ public class EmailServiceImpl implements EmailService {
         message.setSubject(subject);
         message.setText(text);
         emailSender.send(message);
+    }
+
+    @Override
+    public void sendMailToProfessor(Professor professor, String subject, String text) {
+        sendMessage(professor.getEmail(), subject, text);
     }
 
 }
