fa9f21f2c8f5806cd9c60856abb3cf88c3b42591
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-19 15:58:19 -0300

    Simple Exception Handler

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -1,12 +1,26 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 
+
+@ControllerAdvice
 @Controller
 public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest request, Exception exception) {
+
+        final ModelAndView mav = new ModelAndView("error");
+        mav.addObject("exception", exception);
+        mav.addObject("url", request.getRequestURL());
+        return mav;
+    }
 
     @RequestMapping("/403")
     public ModelAndView forbidden() {
