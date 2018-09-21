ed93677a0303d34ef045fe1f3fc9d7023595649d
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-21 17:12:14 -0300

    Fix typo

--- persistence/src/test/java/ar/edu/itba/paw/persistence/ScheduleJdbcDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/ScheduleJdbcDaoTest.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 @Sql("classpath:schema.sql")
-public class SceduleJdbcDaoTest {
+public class ScheduleJdbcDaoTest {
 
 
     @Autowired
