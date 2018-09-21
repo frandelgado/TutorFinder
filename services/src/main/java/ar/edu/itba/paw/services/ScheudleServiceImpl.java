7b1217647f623ba7a0c3501d933717d4279dd08b
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-21 17:14:48 -0300

    Add initial implementation of schedule service

--- services/src/main/java/ar/edu/itba/paw/services/ScheudleServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/ScheudleServiceImpl.java
@@ -1,16 +1,47 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.exceptions.InvalidTimeException;
+import ar.edu.itba.paw.exceptions.InvalidTimeRangeException;
+import ar.edu.itba.paw.interfaces.persistence.ProfessorDao;
+import ar.edu.itba.paw.interfaces.persistence.ScheduleDao;
 import ar.edu.itba.paw.interfaces.service.ScheduleService;
 import ar.edu.itba.paw.models.Professor;
 import ar.edu.itba.paw.models.Timeslot;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Set;
+import java.sql.Time;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ScheudleServiceImpl implements ScheduleService {
 
+    @Autowired
+    ScheduleDao sd;
 
     @Override
-    public Set<Timeslot> reserveTimeSlot(Professor professor, Integer day, Integer startTime, Integer EndTime) {
-        return null;
+    public List<Timeslot> reserveTimeSlot(Professor professor, Integer day, Integer startTime, Integer endTime) throws InvalidTimeException, InvalidTimeRangeException {
+
+        List<Timeslot> reservedTimeslots = new LinkedList<>();
+
+        if(startTime >= endTime)
+            throw new InvalidTimeRangeException();
+        if(startTime > 24 || startTime < 0 || endTime > 24 || endTime < 0)
+            throw  new InvalidTimeException();
+
+        for(int hour = startTime; hour < endTime; hour++ ){
+            reservedTimeslots.add(sd.reserveTimeSlot(professor, day, hour++));
+        }
+
+        return IntStream
+                .range(startTime, endTime)
+                .mapToObj(i -> sd.reserveTimeSlot(professor, day, i))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Timeslot> getScheduleForProfessor(Professor professor) {
+        return sd.getScheduleForProfessor(professor);
     }
 }
