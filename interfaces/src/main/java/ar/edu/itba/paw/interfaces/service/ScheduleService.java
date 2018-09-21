b31337458b27ae64fc55f0201c52b121e05073db
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-21 17:13:28 -0300

    Add exception throwing to method signatures in the interfaces

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ScheduleService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ScheduleService.java
@@ -1,11 +1,16 @@
 package ar.edu.itba.paw.interfaces.service;
 
+import ar.edu.itba.paw.exceptions.InvalidTimeException;
+import ar.edu.itba.paw.exceptions.InvalidTimeRangeException;
 import ar.edu.itba.paw.models.Professor;
 import ar.edu.itba.paw.models.Timeslot;
 
+import java.util.List;
 import java.util.Set;
 
 public interface ScheduleService {
 
-    Set<Timeslot> reserveTimeSlot(Professor professor, Integer day, Integer startTime, Integer EndTime);
+    List<Timeslot> reserveTimeSlot(Professor professor, Integer day, Integer startTime, Integer endTime) throws InvalidTimeException, InvalidTimeRangeException;
+
+    List<Timeslot> getScheduleForProfessor(Professor professor);
 }
