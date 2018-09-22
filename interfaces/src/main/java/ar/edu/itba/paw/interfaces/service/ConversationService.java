51ad4fb4db057cb03f9a42b968e3b393eb31f040
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-21 23:33:45 -0300

    Add ConversationService find methods

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ConversationService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ConversationService.java
@@ -1,13 +1,22 @@
 package ar.edu.itba.paw.interfaces.service;
 
+import ar.edu.itba.exceptions.SameUserConversationException;
+import ar.edu.itba.exceptions.UserNotInConversationException;
 import ar.edu.itba.paw.models.Conversation;
 import ar.edu.itba.paw.models.Professor;
 import ar.edu.itba.paw.models.Subject;
 import ar.edu.itba.paw.models.User;
 
+import java.util.List;
+
 public interface ConversationService {
 
-    public boolean sendMessage(final User user, final Professor professor, final Subject subject, final String body);
+    boolean sendMessage(final User user, final Professor professor, final Subject subject, final String body)
+            throws SameUserConversationException, UserNotInConversationException;
 
-    public boolean sendMessage(final User from, final Conversation conversation, final String body);
+    boolean sendMessage(final User from, final Conversation conversation, final String body) throws UserNotInConversationException;
+
+    List<Conversation> findByUserId(final Long userId);
+
+    Conversation findById(final Long conversation_id, final  Long userId) throws UserNotInConversationException;
 }
--- services/src/main/java/ar/edu/itba/paw/services/ConversationServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/ConversationServiceImpl.java
@@ -1,12 +1,15 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.exceptions.SameUserConversationException;
+import ar.edu.itba.exceptions.UserNotInConversationException;
 import ar.edu.itba.paw.interfaces.persistence.ConversationDao;
 import ar.edu.itba.paw.interfaces.service.ConversationService;
+import ar.edu.itba.paw.interfaces.service.EmailService;
 import ar.edu.itba.paw.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 
 @Service
@@ -16,7 +19,11 @@ public class ConversationServiceImpl implements ConversationService {
     private ConversationDao conversationDao;
 
     @Override
-    public boolean sendMessage(final User user, final Professor professor, final Subject subject, final String body) {
+    public boolean sendMessage(final User user, final Professor professor, final Subject subject, final String body)
+            throws SameUserConversationException, UserNotInConversationException {
+        if(user.getId().equals(professor.getId()))
+            throw new SameUserConversationException();
+
         final Conversation conversation = conversationDao.findByIds(user.getId(), professor.getId(), subject.getId());
 
         if(conversation == null) {
@@ -27,10 +34,31 @@ public class ConversationServiceImpl implements ConversationService {
     }
 
     @Override
-    public boolean sendMessage(final User from, final Conversation conversation, final String body) {
+    public boolean sendMessage(final User from, final Conversation conversation, final String body)
+            throws UserNotInConversationException {
+
+        if(!conversation.belongs(from.getId())) {
+            throw new UserNotInConversationException();
+        }
+
         final Message message = conversationDao.create(from, body, conversation);
 
         return message != null;
+    }
+
+    public List<Conversation> findByUserId(final Long userId) {
+        return conversationDao.findByUserId(userId);
+    }
+
+    @Override
+    public Conversation findById(final Long conversation_id, final Long userId)
+            throws UserNotInConversationException {
+        final Conversation conversation = conversationDao.findById(conversation_id);
+
+        if(!conversation.belongs(userId))
+            throw new UserNotInConversationException();
+
+        return conversation;
     }
 
 }
