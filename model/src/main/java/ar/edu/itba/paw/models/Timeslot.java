d3c8156cc915ce3fe5db88770a4acfbb505e966c
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-22 18:59:43 -0300

    Timeslot Model remove constraints

--- model/src/main/java/ar/edu/itba/paw/models/Timeslot.java
+++ model/src/main/java/ar/edu/itba/paw/models/Timeslot.java
@@ -1,16 +1,9 @@
 package ar.edu.itba.paw.models;
 
-import com.sun.istack.internal.NotNull;
-import javax.validation.constraints.Size;
-
 public class Timeslot {
 
-    @NotNull
-    @Size(min = 1, max = 7)
     private final Integer day;
 
-    @NotNull
-    @Size(max = 24)
     private final Integer hour;
 
     public Timeslot(Integer day, Integer hour) {
