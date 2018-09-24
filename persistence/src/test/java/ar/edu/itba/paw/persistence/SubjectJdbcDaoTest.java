a9f0c7258fbd134b9fd4078e710e251c5e17718d
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-24 05:46:20 -0300

    Add SubjectDao Test Methods

--- persistence/src/test/java/ar/edu/itba/paw/persistence/SubjectJdbcDaoTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/SubjectJdbcDaoTest.java
@@ -14,17 +14,24 @@ import org.springframework.test.jdbc.JdbcTestUtils;
 
 import javax.sql.DataSource;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertNotNull;
+import java.util.List;
+
+import static junit.framework.TestCase.*;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 @Sql("classpath:schema.sql")
 public class SubjectJdbcDaoTest {
 
-    private static final String NAME = "TestName";
-    private static final String DESCRIPTION = "TestDescription";
-    private static final Long AREA_ID = 1l;
+    private static final String NAME = "Algebra";
+    private static final String DESCRIPTION = "Complicado";
+    private static final Long ID = 1L;
+    private static final Long INVALID_ID = 666L;
+    private static final Long AREA_ID = 1L;
+    private static final String AREA_NAME = "matematica";
+    private static final String AREA_DESCRIPTION = "este area es dificil";
+    private static final Long USER_ID = 5L;
 
     @Autowired
     private DataSource ds;
@@ -37,11 +44,11 @@ public class SubjectJdbcDaoTest {
     @Before
     public void setUp(){
         jdbcTemplate = new JdbcTemplate(ds);
-        JdbcTestUtils.deleteFromTables(jdbcTemplate, "subjects");
     }
 
     @Test
     public void testCreate() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "subjects");
         final Subject subject = subjectDao.create(NAME, DESCRIPTION, AREA_ID);
         assertNotNull(subject);
         assertEquals(NAME, subject.getName());
@@ -49,6 +56,61 @@ public class SubjectJdbcDaoTest {
         assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "subjects"));
     }
 
+    @Test
+    public void testFindByIdValid() {
+        final Subject subject = subjectDao.findById(ID).orElse(null);
+        assertNotNull(subject);
+        assertEquals(ID, subject.getId());
+        assertEquals(NAME, subject.getName());
+        assertEquals(DESCRIPTION, subject.getDescription());
+        assertEquals(AREA_ID, subject.getArea().getId());
+        assertEquals(AREA_DESCRIPTION, subject.getArea().getDescription());
+        assertEquals(AREA_NAME, subject.getArea().getName());
+    }
+
+    @Test
+    public void testFindByIdInvalid() {
+        final Subject subject = subjectDao.findById(INVALID_ID).orElse(null);
+        assertNull(subject);
+    }
+
+    @Test
+    public void testFilterSubjectsByNameValid() {
+        final String PARTIAL_NAME = "alg";
+        final int SUBJECTS = 2;
+
+        final List<Subject> subjects = subjectDao.filterSubjectsByName(PARTIAL_NAME);
+        assertNotNull(subjects);
+        assertEquals(SUBJECTS, subjects.size());
+        final Subject subject = subjects.get(0);
+        assertEquals(ID, subject.getId());
+        assertEquals(NAME, subject.getName());
+        assertEquals(DESCRIPTION, subject.getDescription());
+        assertEquals(AREA_ID, subject.getArea().getId());
+        assertEquals(AREA_DESCRIPTION, subject.getArea().getDescription());
+        assertEquals(AREA_NAME, subject.getArea().getName());
+    }
+
+    @Test
+    public void testFilterSubjectsByNameInvalid() {
+        final String PARTIAL_NAME = "InvalidSubjectTest";
+
+        final List<Subject> subjects = subjectDao.filterSubjectsByName(PARTIAL_NAME);
+        assertNotNull(subjects);
+        assertEquals(0, subjects.size());
+    }
+
+    @Test
+    public void testGetAvailableSubjects() {
+        final int REMAINING_SUBJECTS = 2;
+
+        final List<Subject> subjects = subjectDao.getAvailableSubjects(USER_ID);
+        assertNotNull(subjects);
+        assertEquals(REMAINING_SUBJECTS, subjects.size());
+        assertNotEquals(subjects.get(0).getId(), ID);
+        assertNotEquals(subjects.get(1).getId(), ID);
+    }
+    
     @After
     public void tearDown(){
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "areas");
