28358d9cec75f1a034e67d6a5ee8d76f20d3a33f
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-24 05:45:52 -0300

    Add AreaDaoTest

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/AreaJdbcDaoTest.java
@@ -0,0 +1,105 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.interfaces.persistence.AreaDao;
+import ar.edu.itba.paw.models.Area;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.sql.DataSource;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@Sql("classpath:schema.sql")
+public class AreaJdbcDaoTest {
+
+    private static final String NAME = "matematica";
+    private static final String DESCRIPTION = "este area es dificil";
+    private static final String INVALID_NAME = "InvalidTestName";
+    private static final Long ID = 1L;
+    private static final Long INVALID_ID = 666L;
+
+    @Autowired
+    private DataSource ds;
+
+    @Autowired
+    private AreaDao areaDao;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void setUp(){
+        jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+    @Test
+    public void testCreate() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "areas");
+        final Area area = areaDao.create(NAME, DESCRIPTION);
+        assertNotNull(area);
+        assertEquals(NAME, area.getName());
+        assertEquals(DESCRIPTION, area.getDescription());
+        assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "areas"));
+    }
+
+    @Test(expected = DuplicateKeyException.class)
+    public void testCreateInvalid() {
+        final Area area = areaDao.create(NAME, DESCRIPTION);
+        assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "areas"));
+    }
+
+    @Test
+    public void testFindByIdValid() {
+        final Area area = areaDao.findById(ID).orElse(null);
+        assertNotNull(area);
+        assertEquals(ID, area.getId());
+        assertEquals(NAME, area.getName());
+        assertEquals(DESCRIPTION, area.getDescription());
+    }
+
+    @Test
+    public void testFindByIdInvalid() {
+        final Area area = areaDao.findById(INVALID_ID).orElse(null);
+        assertNull(area);
+    }
+
+    @Test
+    public void filterAreasByNameValid() {
+        final List<Area> areas = areaDao.filterAreasByName(NAME);
+        assertNotNull(areas);
+        assertEquals(1, areas.size());
+        final Area area = areas.get(0);
+
+        assertEquals(ID, area.getId());
+        assertEquals(NAME, area.getName());
+        assertEquals(DESCRIPTION, area.getDescription());
+    }
+
+    @Test
+    public void filterAreasByNameInvalid() {
+        final List<Area> areas = areaDao.filterAreasByName(INVALID_NAME);
+        assertNotNull(areas);
+        assertEquals(0, areas.size());
+    }
+
+    @After
+    public void tearDown(){
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "areas");
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+    }
+
+}
