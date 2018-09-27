bec7bc55800710a4e67b776c3885937b629b40d6
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-27 05:29:00 -0300

    Add new implementations with html
    Testing Required!

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/EmailService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/EmailService.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.interfaces.service;
 
+import ar.edu.itba.paw.models.Conversation;
 import ar.edu.itba.paw.models.Professor;
+import ar.edu.itba.paw.models.User;
 
 public interface EmailService {
 
@@ -9,4 +11,8 @@ public interface EmailService {
     public void sendMailToProfessor(Professor professor, String subject, String text);
 
     void sendRegistrationEmail(String email);
+
+    void sendRegistrationEmail(User user);
+
+    void sendContactEmail(User from, User to, Conversation conversation);
 }
--- services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/EmailServiceImpl.java
@@ -1,17 +1,32 @@
 package ar.edu.itba.paw.services;
 
 import ar.edu.itba.paw.interfaces.service.EmailService;
+import ar.edu.itba.paw.models.Conversation;
 import ar.edu.itba.paw.models.Professor;
+import ar.edu.itba.paw.models.User;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
 public class EmailServiceImpl implements EmailService {
 
+    @Value("classpath:WelcomeMail.html")
+    private Resource registrationMail;
+
+    @Value("classpath:ConversationMail.html")
+    private Resource contactMail;
+
     @Autowired
-    public JavaMailSender emailSender;
+    private JavaMailSender emailSender;
 
     @Override
     public void sendEmail(String to, String subject, String text) {
@@ -36,6 +51,50 @@ public class EmailServiceImpl implements EmailService {
         message.setTo(email);
         message.setSubject(REGISTRATION_SUBJECT);
         message.setText(REGISTRATION_BODY);
+        emailSender.send(message);
+    }
+
+    @Override
+    public void sendRegistrationEmail(User user) {
+        final Document doc;
+        try {
+            doc = Jsoup.parse(registrationMail.getInputStream(), "UTF-8", "");
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        Element username = doc.select("td.username").first();
+        username.text("Username: " + user.getUsername());
+        String REGISTRATION_SUBJECT = "Bienvenido a Tu Teoria!";
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(user.getEmail());
+        message.setSubject(REGISTRATION_SUBJECT);
+        message.setText(doc.text());
+        emailSender.send(message);
+    }
+
+    @Override
+    public void sendContactEmail(User from, User to, Conversation conversation) {
+        final Document doc;
+        try {
+            doc = Jsoup.parse(contactMail.getInputStream(), "UTF-8", "");
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        Element element = doc.select("td.name").first();
+        element.text("Nombre: " + from.getName());
+        element = doc.select("td.lastname").first();
+        element.text("Apellido: " + from.getLastname());
+        element = doc.select("td.subject").first();
+        element.text("Materia: " + conversation.getSubject().getName());
+        Element a = doc.select("a.conversation-button").first();
+        a.attr("href", "http://localhost:8080/Conversation?id=" + conversation.getId());
+        final String SUBJECT = "Se han contactado con vos!";
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to.getEmail());
+        message.setSubject(SUBJECT);
+        message.setText(doc.text());
         emailSender.send(message);
     }
 
