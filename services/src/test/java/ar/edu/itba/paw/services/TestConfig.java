f806018206f847a2e778fa74c091125d49cd65a8
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-09-28 18:42:17 -0300

    Added Beans test config

--- services/src/test/java/ar/edu/itba/paw/services/TestConfig.java
+++ services/src/test/java/ar/edu/itba/paw/services/TestConfig.java
@@ -1,9 +1,64 @@
 package ar.edu.itba.paw.services;
 
+import ar.edu.itba.paw.interfaces.persistence.*;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @ComponentScan({ "ar.edu.itba.paw.services"})
 @Configuration
 public class TestConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        return Mockito.mock(JavaMailSender.class);
+    }
+
+    @Bean
+    public AreaDao areaDao() {
+        return Mockito.mock(AreaDao.class);
+    }
+
+    @Bean
+    public ConversationDao conversationDao() {
+        return Mockito.mock(ConversationDao.class);
+    }
+
+    @Bean
+    public CourseDao courseDao() {
+        return Mockito.mock(CourseDao.class);
+    }
+
+    @Bean
+    public PasswordResetTokenDao passwordResetTokenDao() {
+        return Mockito.mock(PasswordResetTokenDao.class);
+    }
+
+    @Bean
+    public ProfessorDao professorDao() {
+        return Mockito.mock(ProfessorDao.class);
+    }
+
+    @Bean
+    public ScheduleDao scheduleDao() {
+        return Mockito.mock(ScheduleDao.class);
+    }
+
+    @Bean
+    public SubjectDao subjectDao() {
+        return Mockito.mock(SubjectDao.class);
+    }
+
+    @Bean
+    public UserDao userDao() {
+        return Mockito.mock(UserDao.class);
+    }
 }
