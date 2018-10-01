8db4742e97297d995afce2f18a434a51d136c8e4
Francisco Delgado <fdelgado@itba.edu.ar>
2018-09-30 23:38:00 -0300

    Refactor searchresults controller into PRG pattern

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/SearchController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/SearchController.java
@@ -8,11 +8,14 @@ import ar.edu.itba.paw.webapp.form.SearchForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.jws.WebParam;
@@ -33,14 +36,9 @@ public class SearchController extends BaseController{
     @Qualifier("areaServiceImpl")
     private AreaService as;
 
-    @RequestMapping("/searchResults")
+    @RequestMapping(value = "/searchResults", method = RequestMethod.GET)
     public ModelAndView search(@RequestParam(value = "page", defaultValue = "1") final int page,
-                               @ModelAttribute("searchForm") final SearchForm form,
-                               final BindingResult errors) throws PageOutOfBoundsException {
-        if(errors.hasErrors()) {
-            RedirectView redirectView = new RedirectView("/");
-            return new ModelAndView(redirectView);
-        }
+                               @ModelAttribute("searchForm") final SearchForm form) throws PageOutOfBoundsException {
 
         final ModelAndView mav = new ModelAndView("searchResults");
         mav.addObject("page", page);
@@ -64,6 +62,31 @@ public class SearchController extends BaseController{
         mav.addObject("search", form.getSearch());
         mav.addObject("type", form.getType());
         return mav;
+    }
+
+    @RequestMapping(value = "/searchResults", method = RequestMethod.POST)
+    public ModelAndView search(@RequestParam(value = "page", defaultValue = "1")final int page,
+                               @Valid @ModelAttribute("searchForm") final SearchForm form,
+                               final BindingResult errors,
+                               final RedirectAttributes redirectAttributes) throws PageOutOfBoundsException {
+
+        if(errors.hasErrors() || !form.validPriceRange() || !form.validTimeRange()) {
+            if(!form.validPriceRange()){
+                errors.rejectValue("maxPrice", "invalidPriceRange");
+            }
+            if(!form.validTimeRange()){
+                errors.rejectValue("endHour", "profile.add_schedule.timeError");
+            }
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.searchForm", errors);
+            redirectAttributes.addFlashAttribute("searchForm", form);
+        }else {
+            redirectAttributes.addFlashAttribute("searchForm", form);
+            redirectAttributes.addFlashAttribute("page", page);
+        }
+        RedirectView redirect = new RedirectView("/searchResults");
+        return new ModelAndView(redirect);
+
+
     }
 
 }
