5759ba309329bc84057be519bb3b81799e0af44f
Francisco Delgado <fdelgado@itba.edu.ar>
2018-10-19 22:03:45 -0300

    Migrate Professor model to JPA

--- model/src/main/java/ar/edu/itba/paw/models/Professor.java
+++ model/src/main/java/ar/edu/itba/paw/models/Professor.java
@@ -1,15 +1,25 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Lob;
+import javax.persistence.Table;
 import java.util.Base64;
 
+@Entity
+@Table(name = "professors")
 public class Professor extends User{
 
+    @Column(length = 512, nullable = false)
     private final String description;
 
+    //TODO: No estoy muy seguro del column definition, revisar si guarda apropiadamente una imagen.
+    @Lob
+    @Column(name = "profile_picture", nullable = false, columnDefinition = "BYTEA")
     private final byte[] picture;
 
-    public Professor(Long id, String username, String name, String lastname, String password, String email, String description, byte[] picture) {
-        super(id, username, name, lastname, password, email);
+    public Professor(String username, String name, String lastname, String password, String email, String description, byte[] picture) {
+        super(username, name, lastname, password, email);
         this.description = description;
         this.picture = picture;
     }
