3a087a8cef029a49a872591fb95b52512c6b7880
Francisco Delgado <fdelgado@itba.edu.ar>
2018-10-20 17:06:46 -0300

    Add hibernate default constructor for professor

--- model/src/main/java/ar/edu/itba/paw/models/Professor.java
+++ model/src/main/java/ar/edu/itba/paw/models/Professor.java
@@ -1,22 +1,27 @@
 package ar.edu.itba.paw.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Lob;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Base64;
+import java.util.List;
 
 @Entity
 @Table(name = "professors")
 public class Professor extends User{
 
     @Column(length = 512, nullable = false)
-    private final String description;
+    private String description;
 
     //TODO: No estoy muy seguro del column definition, revisar si guarda apropiadamente una imagen.
     @Lob
     @Column(name = "profile_picture", nullable = false, columnDefinition = "BYTEA")
-    private final byte[] picture;
+    private byte[] picture;
+
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = false, mappedBy = "professor")
+    private List<Course> courses;
+
+    public Professor(){
+
+    }
 
     public Professor(String username, String name, String lastname, String password, String email, String description, byte[] picture) {
         super(username, name, lastname, password, email);
