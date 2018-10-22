3a9482fe7df788519cda892329d6bbe7d4777d75
Agustin Calatayud <acalatayud@itba.edu.ar>
2018-10-22 05:51:28 -0300

    Implemented some CourseHibernateDao methods

--- persistence/src/main/java/ar/edu/itba/paw/persistence/CourseHibernateDao.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/CourseHibernateDao.java
@@ -6,30 +6,54 @@ import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.Filter;
 import ar.edu.itba.paw.models.Professor;
 import ar.edu.itba.paw.models.Subject;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class CourseHibernateDao implements CourseDao {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public Optional<Course> findByIds(long professor_id, long subject_id) {
-        return Optional.empty();
+        final TypedQuery<Course> query = em.createQuery("from Course as c " +
+                "where c.professor.id = :professor and c.subject.id = :subject", Course.class);
+        query.setParameter("professor", professor_id);
+        query.setParameter("subject", subject_id);
+        final List<Course> courses = query.getResultList();
+        return courses.isEmpty()? Optional.empty(): Optional.ofNullable(courses.get(0));
     }
 
     @Override
     public List<Course> findByProfessorId(long professor_id, int limit, int offset) {
-        return null;
+        final TypedQuery<Course> query = em.createQuery("from Course as c where c.professor.id = :id" +
+                "order by c.professor.id, c.subject.id", Course.class);
+        query.setParameter("id", professor_id);
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+        return query.getResultList();
     }
 
     @Override
     public List<Course> filterByAreaId(long areaId, int limit, int offset) {
-        return null;
+        final TypedQuery<Course> query = em.createQuery("from Course as c where c.subject.area.id = :id" +
+                "order by c.professor.id, c.subject.id", Course.class);
+        query.setParameter("id", areaId);
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+        return query.getResultList();
     }
 
     @Override
     public List<Course> filter(Filter filter, int limit, int offset) {
-        return null;
+        return new LinkedList<>();
     }
 
     @Override
