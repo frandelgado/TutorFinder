ad116ec627e993d3409bd42e3797c93ae09d09f8
Francisco Delgado <fdelgado@itba.edu.ar>
2018-11-09 01:49:46 -0300

    Add initial version of class reservation form

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/ClassReservationForm.java
@@ -0,0 +1,64 @@
+package ar.edu.itba.paw.webapp.form;
+
+public class ClassReservationForm {
+
+    private Long subjectId;
+
+    private Long professorId;
+
+    private Long studentId;
+
+    private Integer day;
+
+    private Integer startHour;
+
+    private Integer endHour;
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+    public Integer getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(Integer startHour) {
+        this.startHour = startHour;
+    }
+
+    public Integer getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(Integer endHour) {
+        this.endHour = endHour;
+    }
+
+    public Long getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(Long subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public Long getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(Long professorId) {
+        this.professorId = professorId;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+}
