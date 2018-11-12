7a14cc62a0c4c494e06baf19088654e9c494d6eb
Francisco Delgado <fdelgado@itba.edu.ar>
2018-11-11 21:58:25 -0300

    Modify classReservation services to handle business logic checks

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ClassReservationService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/service/ClassReservationService.java
@@ -2,6 +2,7 @@ package ar.edu.itba.paw.interfaces.service;
 
 import ar.edu.itba.paw.exceptions.NonexistentProfessorException;
 import ar.edu.itba.paw.exceptions.SameUserException;
+import ar.edu.itba.paw.exceptions.UserAuthenticationException;
 import ar.edu.itba.paw.models.ClassReservation;
 import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.Professor;
@@ -11,12 +12,16 @@ import org.joda.time.LocalDateTime;
 
 public interface ClassReservationService {
 
-    ClassReservation reserve(LocalDateTime startTime, LocalDateTime endTime, Course course, Long studentId) throws SameUserException;
+    ClassReservation reserve(final LocalDateTime startTime, final LocalDateTime endTime,
+                             final Course course, final Long studentId) throws SameUserException;
 
-    ClassReservation confirm(ClassReservation classReservation, String comment);
+    ClassReservation confirm(final Long classReservationId, final Long professorId, final String comment) throws UserAuthenticationException;
 
-    ClassReservation deny(ClassReservation classReservation, String comment);
 
     boolean hasAcceptedReservation(User student, Course course);
+
+    ClassReservation deny(final Long classReservationId, final Long professorId, final String comment) throws UserAuthenticationException;
+
+    ClassReservation findById(final Long classReservationId);
 }
 
