fdbaacaf14f012c96438d0defbda634e69d4d13f
Francisco Delgado <fdelgado@itba.edu.ar>
2018-11-12 15:11:43 -0300

    add test stubs for CourseFileService and CourseFileDao

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/CourseFileHibernateDaoTest.java
@@ -0,0 +1,34 @@
+package ar.edu.itba.paw.persistence;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = HibernateTestConfig.class)
+@Sql("classpath:schema.sql")
+@Transactional
+public class CourseFileHibernateDaoTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private CourseFileHibernateDao cfd;
+
+
+
+
+}
