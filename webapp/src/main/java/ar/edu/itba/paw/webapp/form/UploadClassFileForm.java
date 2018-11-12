2a80f1820d0ccb0cfafbc0dca5d2d8e87c74d60f
Francisco Delgado <fdelgado@itba.edu.ar>
2018-11-12 03:12:21 -0300

    Add controllers and course file upload form

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/UploadClassFileForm.java
@@ -0,0 +1,36 @@
+package ar.edu.itba.paw.webapp.form;
+
+import ar.edu.itba.paw.webapp.validator.FileSize;
+import ar.edu.itba.paw.webapp.validator.FileType;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UploadClassFileForm {
+
+    @NotNull
+    @FileSize(min = 1, max = 81920)
+    @FileType
+    private MultipartFile file;
+
+    @NotNull
+    @Size(min = 5, max = 255)
+    private String description;
+
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
