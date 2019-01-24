edb242cf55fa363e8ec924b1cea8f99151843874
Agustin Calatayud <acalatayud@itba.edu.ar>
2019-01-24 14:21:59 -0300

    Area dtos WIP

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/AreaDTO.java
@@ -0,0 +1,68 @@
+package ar.edu.itba.paw.webapp.dto;
+
+import ar.edu.itba.paw.models.Area;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.net.URI;
+
+@XmlRootElement
+public class AreaDTO {
+
+    private long id;
+    private String name;
+    private String description;
+    private URI url;
+    private URI areaCoursesUrl;
+    //TODO: Imagen
+
+    public AreaDTO() {
+    }
+
+    public AreaDTO(final Area area, final URI baseUri) {
+        this.id = area.getId();
+        this.description = area.getDescription();
+        this.name = area.getName();
+        this.url = baseUri.resolve("areas/" + id);
+        this.areaCoursesUrl = baseUri.resolve("areas/" + id +"/courses");
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public URI getUrl() {
+        return url;
+    }
+
+    public void setUrl(URI url) {
+        this.url = url;
+    }
+
+    public URI getAreaCoursesUrl() {
+        return areaCoursesUrl;
+    }
+
+    public void setAreaCoursesUrl(URI areaCoursesUrl) {
+        this.areaCoursesUrl = areaCoursesUrl;
+    }
+}
